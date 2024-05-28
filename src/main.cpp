dea5cdaf7d5bdb25849c9d8be85c8cfbcfe6e09a
Jason Armstrong <jasnarmstrng@gmail.com>
2024-05-27 22:24:08 -0400

    feat: Change category map -> conversion factor map
    The former category map now has a another attribute for each category of
    units - a conversion factor. The new conversion factor map is a map of
    maps.
    
    Additionally:
    refactor: Remove unused initialization functions.
    test: Regression test completed. No breaking after changes to map
    structure and the update of map references.

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,115 @@
 #include <unordered_map>
 #include <vector>
 
-// Globally accessed static maps.
-// Can I move this to a header file?
-static std::unordered_map<std::string, std::vector<std::string> > categories =
+// Covert static std::unordered_map<std::string, std::vector<std::string> > categories into a map that
+// allows for the unit conversion factors to be defined in a category.
+using conversion_factor_map = std::unordered_map<std::string, std::unordered_map<std::string, double> >;
+static conversion_factor_map conversion_factors =
+// To do:
+// 1. Establish the base unit of each category. We will first convert our <unit_from> to the base
+// unit then convert to the <unit_to> via its conversion factor. This is possible because of the linear
+// scaling relationship between the units of the categories.
+//
+// 2. Add the conversion factors.
+//
+// 3. Move the maps and functions to a header or shared objects file. I don't know if shared object file
+// is the right file I'm thinking should be used here.
 {
-    {"TEMPERATURE", { "celsius", "fahrenheit", "Kelvin" }},
-    {"LENGTH",      { "nanometer", "millimeter", "centimeter", "meter", "kilometer", "inch", "feet", "yard", "furlong", "mile", "league", "nautical mile" }},
-    {"MASS",        { "milligram", "gram", "kilogram", "tonne", "ounce", "pound", "stone" }},
-    {"VOLUME",      { "milliliter", "centiliter", "liter", "fluid oz.", "gallon" }},
-    {"DATA",        { "Bit", "Byte", "KiloByte", "MegaByte", "GigaByte", "TeraByte", "PetaByte" }},
-    {"TIME",        { "nanosecond", "millisecond", "second", "minute", "hour", "day", "week", "year" }},
-    {"ENERGY",      { "Joule", "calorie", "Watt", "kilowatt", "BTU", "Quad", "therm", "horsepower" }},
-    {"PRESSURE",    { "Pascal", "atmosphere", "torr", "psi" }},
-    {"ANGLE",       { "degrees", "radians" }}
+    // Category
+    {"TEMPERATURE",{
+        // Unit ... Conversion factor
+        {"celsius", 0.0},
+        {"fahrenheit", 0.0},
+        {"Kelvin", 0.0}
+    }},
+    {"LENGTH",{
+        {"nanometer", 0.0},
+        {"millimeter", 0.0},
+        {"centimeter", 0.0},
+        {"meter", 0.0},
+        {"kilometer", 0.0},
+        {"inch", 0.0},
+        {"feet", 0.0},
+        {"yard", 0.0},
+        {"furlong", 0.0},
+        {"mile", 0.0},
+        {"league", 0.0},
+        {"nautical mile", 0.0}
+    }},
+    {"MASS",{
+        {"milligram", 0.0},
+        {"gram", 0.0},
+        {"kilogram", 0.0},
+        {"tonne", 0.0},
+        {"ounce", 0.0},
+        {"pound", 0.0},
+        {"stone", 0.0}
+    }},
+    {"VOLUME",{
+        {"milliliter", 0.0},
+        {"centiliter", 0.0},
+        {"liter", 0.0},
+        {"fluid oz.", 0.0},
+        {"gallon", 0.0}
+    }},
+    {"DATA",{
+        {"Bit", 0.0},
+        {"Byte", 0.0},
+        {"KiloByte", 0.0},
+        {"MegaByte", 0.0},
+        {"GigaByte", 0.0},
+        {"TeraByte", 0.0},
+        {"PetaByte", 0.0}
+    }},
+    {"TIME",{
+        {"nanosecond", 0.0},
+        {"millisecond", 0.0},
+        {"second", 0.0},
+        {"minute", 0.0},
+        {"hour", 0.0},
+        {"day", 0.0},
+        {"week", 0.0},
+        {"year", 0.0}
+    }},
+    {"ENERGY",{
+        {"Joule", 0.0},
+        {"calorie", 0.0},
+        {"Watt", 0.0},
+        {"kilowatt", 0.0},
+        {"BTU", 0.0},
+        {"Quad", 0.0},
+        {"therm", 0.0},
+        {"horsepower", 0.0}
+    }},
+    {"PRESSURE",{
+        {"Pascal", 0.0},
+        {"atmosphere", 0.0},
+        {"torr", 0.0},
+        {"psi", 0.0}
+    }},
+    {"ANGLE",{
+        {"degrees", 0.0},
+        {"radians", 0.0}
+    }}
 };
 
 static std::unordered_map<std::string, std::vector<std::string> > constants =
 {
-    // Constant           Value                Note/Measurement Unit
-    // -------------------------------------------------------------
-    {"SPEED OF LIGHT", { "299792458",         "[meters/second]" }},
-    {"PI",             { "3.141592653589793", "" }},
-    {"ROOT 2",         { "1.414213562373095", "" }},
-    {"PHI",            { "1.618033988749894", "" }},
-    {"TAU",            { "6.283185307179586", "" }},
-    {"E",              { "2.718281828459045", "" }}
+    // Constant name
+    {"SPEED OF LIGHT",
+        // Value ...... Note or measurement unit
+        { "299792458", "[meters/second]" }
+    },
+    {"PI", { "3.141592653589793", "" }},
+    {"ROOT 2", { "1.414213562373095", "" }},
+    {"PHI", { "1.618033988749894", "" }},
+    {"TAU", { "6.283185307179586", "" }},
+    {"E", { "2.718281828459045", "" }}
 };
 
 // Function declarations.
 // To do: Move function decs and defs to a header file.
 std::string to_upper(const std::string& str);
-void initialize_categories_map();
-void initialize_constants_map();
 void print_usage(void);  // Function to print program usage information
 void display_categories();
 void display_units(const std::string& category);
@@ -76,18 +153,18 @@ void print_usage()
 void display_categories()
 {
     std::cout << "Available unit categories:" << std::endl;
-    for (const auto& [key, _] : categories)
+    for (const auto& [key, _] : conversion_factors)
         std::cout << " - " << key << std::endl;
 }
 
 void display_units(const std::string& category)
 {
     std::string cat = to_upper(category);
-    if (categories.find(cat) != categories.end())
+    if (conversion_factors.find(cat) != conversion_factors.end())
     {
         std::cout << "Available units in the " << cat << " category:" << std::endl;
-        for (const auto& value : categories[cat])
-            std::cout << " - " << value << std::endl;
+        for (const auto& value : conversion_factors[cat])
+            std::cout << " - " << value.first << std::endl;
     }
     else
         std::cout << "Unknown category: " << category << std::endl;
